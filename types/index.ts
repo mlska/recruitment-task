4d7c256d839ae33893ed6e46ffa957c70bebf1e3
Kamil <kamil.malesa1@gmail.com>
2022-12-27 20:23:53 +0100

    added message type

---
+++ types/index.ts
@@ -0,0 +1,6 @@
+export enum ValidationError {
+  firstName = "First name field is blank",
+  lastName = "Last name field is blank",
+  email = "Wrong email address",
+  date = "Date is not picked",
+}
