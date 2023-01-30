e8390736085e92f17050db14b0d75905f7b82f70
Kamil <kamil.malesa1@gmail.com>
2023-01-30 19:21:22 +0100

    feat: new error messages

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,7 @@
-export enum ValidationError {
+export enum Error {
   firstName = "First name field is blank",
   lastName = "Last name field is blank",
   email = "Wrong email address",
   date = "Date is not picked",
+  connection = "Error cannot insert event",
 }
