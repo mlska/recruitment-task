e934244e6aec86770226049c3090797c9a435356
Kamil <kamil.malesa1@gmail.com>
2023-01-30 19:21:40 +0100

    feat: deleting events

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -5,11 +5,40 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const prisma = new PrismaClient();
 
-  const events = await prisma.event.findMany();
+  console.log(req.method);
+  switch (req.method) {
+    case "GET":
+      const events = await prisma.event.findMany();
+      if (events?.length) {
+        res.status(200).json(events);
+      } else {
+        res.status(404).json("Error in reading events");
+      }
+      break;
 
-  if (events?.length) {
-    return res.send({ status: "200", events });
+    case "PUT":
+      const data = JSON.parse(req.body);
+      const event = await prisma.event.create({
+        data,
+      });
+      res.status(200).json(event);
+      break;
+
+    case "PATCH":
+      res.status(200).json({ message: "PATCH" });
+      break;
+
+    case "DELETE":
+      const id = JSON.parse(req.body);
+      const record = await prisma.event.delete({
+        where: {
+          id,
+        },
+      });
+      res.status(200).json(record);
+      break;
+
+    default:
+      res.status(405).json({ message: "Wrong method" });
   }
-
-  res.send({ status: "404", message: "events not found" });
 };
